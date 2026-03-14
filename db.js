e5478f9c0f029af0bd2be0da1b9b7b0145fbbd12
OukiDEV <wendytanuwijaya93@gmail.com>
2026-03-14 14:33:56 +0700

    fix: remove SSL, pakai MySQL lokal

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 const mysql = require('mysql2');
-const fs = require('fs');
-const path = require('path');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -9,23 +7,16 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {
-        ca: fs.existsSync(path.join(__dirname, 'ca.pem'))
-            ? fs.readFileSync(path.join(__dirname, 'ca.pem'))
-            : undefined,
-        rejectUnauthorized: true
-    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Test koneksi saat startup
 pool.getConnection((err, connection) => {
     if (err) {
-        console.error('[DB] Gagal koneksi ke Aiven MySQL:', err.message);
+        console.error('[DB] Gagal koneksi:', err.message);
     } else {
-        console.log('[DB] Berhasil terhubung ke Aiven MySQL!');
+        console.log('[DB] Berhasil terhubung ke MySQL lokal!');
         connection.release();
     }
 });
